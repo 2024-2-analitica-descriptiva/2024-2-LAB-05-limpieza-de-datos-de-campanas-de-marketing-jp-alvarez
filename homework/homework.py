ecf47526fdcbb5a3587a531a2facc816d65d134d
jp-alvarez <jualvarez@unal.edu.co>
2025-01-22 23:28:09 -0500

    intento

--- homework/homework.py
+++ homework/homework.py
@@ -50,8 +50,50 @@ def clean_campaign_data():
 
     """
 
-    return
+    import glob
+    import pandas as pd
+    import os
+
+    input_dir = 'files/input'
+    input_files = glob.glob(f"{input_dir}/*")
+    
+    data_frames = [
+        pd.read_csv(
+            file,
+            delimiter=",",
+            index_col=0,
+        )
+        for file in input_files
+    ]
+
+    combined_df = pd.concat(data_frames, ignore_index=True)
+    print(combined_df.head())
+    print(combined_df.columns)
+
+    os.makedirs('files/output', exist_ok=True)
+
+    client_data = combined_df[['client_id', 'age', 'job', 'marital', 'education', 'credit_default', 'mortgage']].copy()
+    client_data['job'] = client_data['job'].str.replace(".", "", regex=False).str.replace("-", "_", regex=False)
+    client_data["education"] = client_data["education"].str.replace(".", "_", regex=False).replace("unknown", pd.NA)
+    client_data["credit_default"] = client_data["credit_default"].apply(lambda x: 1 if x == "yes" else 0)
+    client_data["mortgage"] = client_data["mortgage"].apply(lambda x: 1 if x == "yes" else 0)
+
+    campaign_data = combined_df[['client_id', 'number_contacts', 'contact_duration', 'previous_campaign_contacts', 'previous_outcome', 'campaign_outcome']]
+    campaign_data['previous_outcome'] = campaign_data['previous_outcome'].apply(lambda x: 1 if x == 'success' else 0)
+    campaign_data['campaign_outcome'] = campaign_data['campaign_outcome'].apply(lambda x: 1 if x == 'yes' else 0)
+    campaign_data['last_contact_date'] = pd.to_datetime('2022-' + combined_df['month'].astype(str) + '-' + combined_df['day'].astype(str), format="%Y-%b-%d")
+    
+    economics_data = combined_df[['client_id', 'cons_price_idx', 'euribor_three_months']].copy()
+
+    client_data.to_csv('files/output/client.csv', index=False)
+    campaign_data.to_csv('files/output/campaign.csv', index=False)
+    economics_data.to_csv('files/output/economics.csv', index=False)
+
 
 
 if __name__ == "__main__":
     clean_campaign_data()
+
+
+
+
